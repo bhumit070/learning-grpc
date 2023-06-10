d52f6538f56fc559cdba8bf9589259ff5e4859c8
bhumit070 <ganatrabhoomit070@gmail.com>
2023-06-10 20:33:07 +0530

    Refactor

---
+++ comments/src/grpc.ts
@@ -0,0 +1,52 @@
+import * as grpc from '@grpc/grpc-js';
+import * as protoLoader from '@grpc/proto-loader';
+import { newPostAdded } from '.';
+
+const protoOptions = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+};
+
+const protoFiles = ['comments.proto', 'posts.proto'].map((protoFile) => {
+    return __dirname + `/proto/${protoFile}`;
+});
+
+const packageDefinitions = protoLoader.loadSync(protoFiles, protoOptions);
+const { commentsProto, postsProto } = grpc.loadPackageDefinition(
+    packageDefinitions,
+) as any;
+
+const server = new grpc.Server();
+
+server.addService(postsProto.PostsService.service, {
+    CreatePost: function (call: any, callback: Function) {
+        const response = {
+            message: 'Post created successfully!',
+            success: true,
+        };
+        newPostAdded(call.request.postId);
+        callback(null, response);
+    },
+});
+
+const commentsService = new commentsProto.CommentsService(
+    'localhost:5000',
+    grpc.credentials.createInsecure(),
+);
+
+const GRPC_PORT = process.env.GRPC_PORT || 5001;
+
+server.bindAsync(
+    `localhost:${GRPC_PORT}`,
+    grpc.ServerCredentials.createInsecure(),
+    (err, port) => {
+        if (err) {
+            process.exit(1);
+        }
+        server.start();
+        console.log(`gRPC server running at http://localhost:${port}`);
+    },
+);
--- comments/src/index.ts
+++ comments/src/index.ts
@@ -1,83 +1,30 @@
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
-import * as grpc from '@grpc/grpc-js';
-import * as protoLoader from '@grpc/proto-loader';
-import console from 'console';
+import './grpc';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 
-const protoOptions = {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true,
-};
+type Comment = Record<string, Array<string>>;
 
-const protoFiles = ['comments.proto', 'posts.proto'].map((protoFile) => {
-    return __dirname + `/proto/${protoFile}`;
-});
+let postComments: Comment = {};
 
-const packageDefinitions = protoLoader.loadSync(protoFiles, protoOptions);
-const { commentsProto, postsProto } = grpc.loadPackageDefinition(
-    packageDefinitions,
-) as any;
-
-const commentsService = new commentsProto.CommentsService(
-    'localhost:5000',
-    grpc.credentials.createInsecure(),
-);
-
-const server = new grpc.Server();
-
-type Comment = {
-    postId: number;
-    comments: Array<string>;
-};
-
-let postComments: Array<Comment> = [];
-
-server.addService(postsProto.PostsService.service, {
-    CreatePost: function (call: any, callback: Function) {
-        console.log(call.request);
-        const response = {
-            message: 'Post created successfully!',
-            success: true,
-        };
-        postComments.push({
-            postId: call.request.postId,
-            comments: [],
-        });
-        callback(null, response);
-    },
-});
+export function newPostAdded(postId: string) {
+    postComments[postId] = [];
+}
 
 const PORT = process.env.PORT || 3001;
-const GRPC_PORT = process.env.GRPC_PORT || 5001;
 
 app.get('/comments', (req, res) => {
     return res.status(200).json({
         data: postComments,
-        total: postComments.length,
+        total: Object.keys(postComments).length,
     });
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-server.bindAsync(
-    `localhost:${GRPC_PORT}`,
-    grpc.ServerCredentials.createInsecure(),
-    (err, port) => {
-        if (err) {
-            process.exit(1);
-        }
-        server.start();
-        console.log(`gRPC server running at http://localhost:${port}`);
-    },
-);
